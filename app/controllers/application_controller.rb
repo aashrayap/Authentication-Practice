3c812ef752abd4d0555d03714ff597529d966369
aashraya <aashraya_p@hotmail.com>
2016-09-05 11:54:27 -0600

    completed sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  # USERS= {"foo"=> "bar"}
+
+
+  # before_action :authenticate_user
+  # private
+  # def authenticate_user
+  #   authenticate_or_request_with_http_digest do |username, password|
+  #     USERS[username]
+#   end
+# end
 end
